169bd94d7b308d8948a5c2dc3bcf4dfe50672059
charly hue <charly.hue@gmail.com>
2017-08-05 11:37:43 +0200

    Add git branch

--- zprompt.sh
+++ zprompt.sh
@@ -5,9 +5,13 @@
 ##########
 
 domain="localdomain"
+git=true
+##########
+##########
 
-##########
-##########
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
 
 
 host=$HOSTNAME
@@ -16,4 +20,12 @@ then
 	host=${host:0:-${#domain}}	
 fi
 
-PS1='\[\e[90m\][$env]\[\e[0m\][ \[\e[0;32m\]\u@$host \[\e[34m\]\w \[\e[m\]]\$ '
+PS1='\[\e[90m\][$env]\[\e[0m\][ \[\e[0;32m\]\u@$host \[\e[34m\]\w'
+
+if [ $git ]
+then
+	PS1=$PS1"\[\e[33m\]"$(parse_git_branch)
+fi
+
+PS1=$PS1' \[\e[m\]]\$ '
+
