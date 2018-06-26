fd722130261c39857c805fb3ce61b77a6ace551a
charly hue <30727779+charlyhue@users.noreply.github.com>
2018-06-26 15:14:55 +0200

    Fix git branch display

--- zprompt.sh
+++ zprompt.sh
@@ -24,7 +24,7 @@ PS1='\[\e[90m\][$env]\[\e[0m\][ \[\e[0;32m\]\u@$host \[\e[34m\]\w'
 
 if [ $git ]
 then
-	PS1=$PS1"\[\e[33m\]"$(parse_git_branch)
+	PS1=$PS1"\[\e[33m\]\$(parse_git_branch)"
 fi
 
 PS1=$PS1' \[\e[m\]]\$ '
